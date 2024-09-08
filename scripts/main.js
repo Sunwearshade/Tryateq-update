490cf44833f6feb07495e2ed86c92d7a0bdc102f
JonathanGP <arrobaminilic5.7@gmail.com>
2024-09-08 14:18:52 -0600

    se actualizo la logica de un boton

--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('whatsapp-btn').addEventListener('click', function() {
-    const chatContainer = document.getElementById('chat-container');
-    if (chatContainer.style.display === 'none') {
-        chatContainer.style.display = 'block';  
-    } else {
-        chatContainer.style.display = 'none';  
-    }
-});
