c32bb89d1644d64a07c7f23d769f3f31e9619436
JonathanGP <arrobaminilic5.7@gmail.com>
2024-09-14 13:22:57 -0600

    se corrigio la orientacion del boton asi como su desplazamiento por la pantalla

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
 document.getElementById('whatsapp-btn').addEventListener('click', function() {
     const chatContainer = document.getElementById('chat-container');
+    
     if (chatContainer.style.display === 'none') {
         chatContainer.style.display = 'block';  
+        
+        chatContainer.scrollIntoView({ behavior: 'smooth' });
     } else {
         chatContainer.style.display = 'none';  
     }
