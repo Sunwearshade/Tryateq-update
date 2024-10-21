299a7285399447098279167f536649829168e941
JonathanGP <Arrobaminilic5.7@gmail.com>
2024-10-20 19:28:44 -0600

    se agregaron selectores al modulo de estilos principales

--- scripts/serviceslist.js
+++ scripts/serviceslist.js
@@ -1,15 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const items = document.querySelectorAll('.list-item');
-    let index = 0;
-  
-    function highlightItem() {
-      if (index < items.length) {
-        items[index].classList.add('active'); 
-        index++;
-        setTimeout(highlightItem, 1000); 
+  const items = document.querySelectorAll('.list-item');
+  let index = 0;
+
+  function highlightItem() {
+    if (index < items.length) {
+      const currentItem = items[index];
+      currentItem.classList.add('active'); 
+
+      const palomitasContainer = currentItem.querySelector('.palomitas');
+      if (palomitasContainer) {
+        setTimeout(() => {
+          palomitasContainer.classList.remove('hidden'); 
+          palomitasContainer.style.opacity = 1; 
+        }, 500); 
       }
+
+      index++;
+      setTimeout(highlightItem, 1000); 
     }
-  
-    setTimeout(highlightItem, 3000);
-  });
-  
+  }
+
+  setTimeout(highlightItem, 3000); 
+});
